58a5651b978067030fad234d256b9dcfc5761b89
unknown <rafaelantoniassivicechio@gmail.com>
2023-02-12 03:58:27 -0300

    Add header content

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,27 @@
 addEventListener("DOMContentLoaded", () => {
-  const navToggle = document.querySelector(".header__nav-toggle");
-  const primaryNav = document.querySelector(".header__nav-navigation");
-  const talkBtn = document.querySelector(".header__nav-talk");
+  const navToggle = document.querySelector(".navigation__toggle");
+  const primaryNav = document.querySelector(".navigation__navigation");
+  const talkBtn = document.querySelector(".navigation__talk");
 
   navToggle.addEventListener("click", () => {
-    primaryNav.hasAttribute("data-visible") ? navToggle.setAttribute("aria-expanded", false) : navToggle.setAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible");
-    talkBtn.toggleAttribute("data-visible");
+    if (talkBtn.hasAttribute("data-visible")) {
+      
+      primaryNav.toggleAttribute("data-invisible");
+      talkBtn.toggleAttribute("data-invisible");
+      
+      primaryNav.removeAttribute("data-visible");
+      talkBtn.removeAttribute("data-visible");
+      
+      navToggle.setAttribute("aria-expanded", false);
+    }
+    else {
+      primaryNav.toggleAttribute("data-visible");
+      talkBtn.toggleAttribute("data-visible");
+
+      primaryNav.removeAttribute("data-invisible");
+      talkBtn.removeAttribute("data-invisible");
+      
+      navToggle.setAttribute("aria-expanded", true);
+    } 
   });
 });
