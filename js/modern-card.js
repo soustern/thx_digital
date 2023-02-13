4e3dc8c9c59e4ad922b7e5cbae157f4fb79f8fad
unknown <rafaelantoniassivicechio@gmail.com>
2023-02-13 04:04:03 -0300

    Add services section

---
+++ js/modern-card.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  document.getElementById("card").onmousemove = (e) => {
+    for (const card of document.getElementsByClassName("modern-card")) {
+      const rect = card.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    }
+  };
+};
