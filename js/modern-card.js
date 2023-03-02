104b740ebcfd9eb259f996ee08ebb33193560f89
unknown <rafaelantoniassivicechio@gmail.com>
2023-03-02 03:14:01 -0300

    Redo cards, start new layout

--- js/modern-card.js
+++ js/modern-card.js
@@ -1,5 +1,16 @@
 window.onload = function () {
-  document.getElementById("card").onmousemove = (e) => {
+  document.getElementById("numbers").onmousemove = (e) => {
+    for (const card of document.getElementsByClassName("modern-card")) {
+      const rect = card.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    }
+  };
+
+  document.getElementById("services").onmousemove = (e) => {
     for (const card of document.getElementsByClassName("modern-card")) {
       const rect = card.getBoundingClientRect(),
         x = e.clientX - rect.left,
